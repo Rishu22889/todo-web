7e6ee0c114a0f77787e7bb97176f9981576cb34f
Rishu <rk1258106@gmail.com>
2025-07-17 17:54:15 +0530

    Initial Commit

--- static/js/test.js
+++ static/js/test.js
@@ -1,1 +1,25 @@
-console.log("rishi")
+const quotes = [
+      "\"Small steps every day.\"",
+      "\"Do it now, not later.\"",
+      "\"Progress over perfection.\"",
+      "\"What gets scheduled gets done. — Robin Sharma\"",
+      "\"You've got this.\"",
+      "\"One task at a time, one step closer.\"",
+      "\"Don't count the days, make the days count. — Muhammad Ali\""
+    ];
+
+const quoteEl = document.getElementById("quote");
+let index = 0;
+
+function showNextQuote() {
+    quoteEl.style.opacity = 0;
+
+    setTimeout(() => {
+        quoteEl.textContent = quotes[index];
+        quoteEl.style.opacity = 1;
+        index = (index + 1) % quotes.length;
+    }, 500);
+}
+showNextQuote();
+
+setInterval(showNextQuote, 5000);
